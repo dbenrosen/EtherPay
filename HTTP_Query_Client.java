e66092c5bb8cff02deec0bffe4dda7fdec85bd87
dbenrosen <dbenrosen@mcon.org>
2016-11-28 15:27:21 -0800

    Delete HTTP_Query_Client.java

--- HTTP_Query_Client.java
+++
@@ -1,9 +0,0 @@
-package com.bringcommunications.etherpay;
-
-/**
- * Created by dbrosen on 11/25/16.
- */
-
-public interface HTTP_Query_Client {
-    public void handle_http_rsp(String callback, String rsp);
-}
